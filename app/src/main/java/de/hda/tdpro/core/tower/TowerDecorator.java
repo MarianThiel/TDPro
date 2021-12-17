4180d125b7fe207632c0374b7444bc5ccc46cb21
Marian Thiel <stmithie@stud.h-da.de>
2021-12-17 13:55:30 +0100

    overridden methods in TowerDecorator

--- app/src/main/java/de/hda/tdpro/core/tower/TowerDecorator.java
+++ app/src/main/java/de/hda/tdpro/core/tower/TowerDecorator.java
@@ -6,4 +6,24 @@ public class TowerDecorator extends Tower {
         super(0,0,0,0);
         embeddedTower = uTower;
     }
+
+    @Override
+    public int getDamage() {
+        return this.damage + super.getDamage();
+    }
+
+    @Override
+    public int getRadius() {
+        return this.radius + super.getRadius();
+    }
+
+    @Override
+    public float getSpeed() {
+        return this.speed + super.getSpeed();
+    }
+
+    @Override
+    public int getPrice() {
+        return this.price + super.getPrice();
+    }
 }
