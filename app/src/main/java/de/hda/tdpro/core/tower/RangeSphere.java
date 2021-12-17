659f29497e81e0dc3c98edc68c25ed1ab58922be
Marian Thiel <stmithie@stud.h-da.de>
2021-12-17 09:46:33 +0100

    added class Tower and RangeSphere

---
+++ app/src/main/java/de/hda/tdpro/core/tower/RangeSphere.java
@@ -0,0 +1,14 @@
+package de.hda.tdpro.core.tower;
+
+public class RangeSphere {
+
+    private int range;
+
+    public RangeSphere(int range) {
+        this.range = range;
+    }
+
+    public void hitEnemy(int dmg){
+
+    }
+}
---
+++ app/src/main/java/de/hda/tdpro/core/tower/Tower.java
@@ -0,0 +1,61 @@
+package de.hda.tdpro.core.tower;
+
+/**
+ * @author Marian Thiel
+ *  Abstract Class representing a tower
+ */
+abstract public class Tower {
+
+    protected int radius;
+    protected int damage;
+    protected float speed;
+    protected int price;
+
+    RangeSphere sphere;
+
+
+    public Tower(int radius, int damage, float speed, int price) {
+        this.radius = radius;
+        this.damage = damage;
+        this.speed = speed;
+        this.price = price;
+
+        sphere = new RangeSphere(radius);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void fireMissile(){
+        sphere.hitEnemy(damage);
+    }
+}
