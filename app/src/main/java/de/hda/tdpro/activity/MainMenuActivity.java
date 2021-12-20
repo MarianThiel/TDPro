ee6c34af2b360ad04beb73a8057f81469b3af3e9
Felix <felix.ruff@protonmail.com>
2021-12-20 19:12:33 +0100

    Main Menu Level Selection

--- app/src/main/java/de/hda/tdpro/activity/MainMenuActivity.java
+++ app/src/main/java/de/hda/tdpro/activity/MainMenuActivity.java
@@ -1,15 +1,63 @@
 package de.hda.tdpro.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import de.hda.tdpro.R;
+import pl.droidsonroids.gif.GifImageButton;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GifImageButton levelSelect = (GifImageButton) findViewById(R.id.buttonLevelSelection);
+        levelSelect.setOnClickListener((View.OnClickListener) this);
+        GifImageButton spellSelect = (GifImageButton) findViewById(R.id.buttonSpellSelection);
+        spellSelect.setOnClickListener((View.OnClickListener) this);
+        GifImageButton towerSelect = (GifImageButton) findViewById(R.id.buttonTowerSelection);
+        towerSelect.setOnClickListener((View.OnClickListener) this);
+        ImageButton lexikonSelect = (ImageButton) findViewById(R.id.imageButtonLexikon);
+        lexikonSelect.setOnClickListener((View.OnClickListener) this);
+        ImageButton settingsSelect = (ImageButton) findViewById(R.id.imageButtonSettings);
+        settingsSelect.setOnClickListener((View.OnClickListener) this);
+        Button switchbutton = (Button) findViewById(R.id.buttonswitch);
+        switchbutton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view) {
+        GifImageButton levelSelect = findViewById(R.id.buttonLevelSelection);
+        GifImageButton spellSelect = findViewById(R.id.buttonSpellSelection);
+        GifImageButton towerSelect = findViewById(R.id.buttonTowerSelection);
+        ImageButton settingsSelect = findViewById(R.id.imageButtonSettings);
+        ImageButton lexikonSelect = findViewById(R.id.imageButtonLexikon);
+        Button switchbutton = findViewById(R.id.buttonswitch);
+        if (view == levelSelect) {
+            Intent fp = new Intent(getApplicationContext(), LevelSelectionActivity.class);
+            startActivity(fp);
+        }
+        if (view == spellSelect) {
+            Intent fp = new Intent(getApplicationContext(), AbilityUpgradeActivity.class);
+            startActivity(fp);
+        }
+        if (view == towerSelect) {
+            Intent fp = new Intent(getApplicationContext(), TowerUpgradeActivity.class);
+            startActivity(fp);
+        }
+        if (view == settingsSelect) {
+            Intent fp = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(fp);
+        }
+        if (view == lexikonSelect) {
+            Intent fp = new Intent(getApplicationContext(), LexiconActivity.class);
+            startActivity(fp);
+        }
+
     }
 }
+
