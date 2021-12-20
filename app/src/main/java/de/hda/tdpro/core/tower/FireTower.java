0a5e9c73d2cf10ff60a96e7fdeb5a7f9b1e2dc21
Marian Thiel <stmithie@stud.h-da.de>
2021-12-20 17:06:27 +0100

    init of sphere in Concrete Tower

--- app/src/main/java/de/hda/tdpro/core/tower/FireTower.java
+++ app/src/main/java/de/hda/tdpro/core/tower/FireTower.java
@@ -3,5 +3,6 @@ package de.hda.tdpro.core.tower;
 public class FireTower extends Tower {
     public FireTower(int radius, int damage, float speed, int price) {
         super(radius, damage, speed, price);
+        this.sphere = new RangeSphere(this, radius);
     }
 }
