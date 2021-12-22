64873ed3de4e049f22aa4abf24897b9f98ecef39
Marian Thiel <stmithie@stud.h-da.de>
2021-12-22 14:00:42 +0100

    implemented Wave created DemoGame

--- app/src/main/java/de/hda/tdpro/core/enemy/EnemyWave.java
+++ app/src/main/java/de/hda/tdpro/core/enemy/EnemyWave.java
@@ -1,12 +1,42 @@
 package de.hda.tdpro.core.enemy;
 
-public class EnemyWave {
+public class EnemyWave implements Runnable {
 
     private final int ENEMIES_IN_WAVE;
 
     private final Enemy[] enemies;
-    public EnemyWave(int ENEMIES_IN_WAVE) {
+
+    private Thread thread;
+
+    private int lastEnemyPosition;
+    private final Path mainPath;
+
+    public EnemyWave(int ENEMIES_IN_WAVE, Path path) {
         this.ENEMIES_IN_WAVE = ENEMIES_IN_WAVE;
         enemies = new Enemy[ENEMIES_IN_WAVE];
+        mainPath = path;
+        lastEnemyPosition = 0;
+    }
+
+    public void addEnemy(Enemy e){
+        e.setWalkingPath(mainPath);
+        enemies[lastEnemyPosition++] = e;
+    }
+
+    public void startWave(){
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        for(Enemy e : enemies){
+            e.initWalking();
+            try {
+                Thread.sleep((long) (Math.random() * 1000));
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+        }
     }
 }
