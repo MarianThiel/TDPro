ef59071c5abf70c4560571a3cb22a23bd7909b3a
Marian Thiel <stmithie@stud.h-da.de>
2021-12-22 14:00:42 +0100

    implemented Wave created DemoGame

---
+++ app/src/main/java/de/hda/tdpro/core/DemoGame.java
@@ -0,0 +1,10 @@
+package de.hda.tdpro.core;
+
+import android.graphics.Canvas;
+
+public class DemoGame implements Drawable{
+    @Override
+    public void draw(Canvas canvas) {
+        
+    }
+}
