c83e5058dc07fe137243ed329b03203067345fc5
MarianThiel <MarianThiel@users.noreply.github.com>
2021-12-26 15:08:40 +0100

    refactored Tower and implementations to handle context drawing - thread behaviour changed to not start/stop thread while enemy is/isn't in range

--- app/src/main/java/de/hda/tdpro/core/tower/FireTower.java
+++ app/src/main/java/de/hda/tdpro/core/tower/FireTower.java
@@ -1,8 +1,21 @@
 package de.hda.tdpro.core.tower;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+import de.hda.tdpro.R;
+
 public class FireTower extends Tower {
-    public FireTower(int radius, int damage, float speed, int price) {
-        super(radius, damage, speed, price);
+    public FireTower(int radius, int damage, float speed, int price, Context context) {
+        super(radius, damage, speed, price, context);
         this.sphere = new RangeSphere(this, radius);
+        img = BitmapFactory.decodeResource(context.getResources(), R.drawable.ft_test);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawBitmap(img, getPos().getxVal(),getPos().getyVal(),null);
     }
 }
