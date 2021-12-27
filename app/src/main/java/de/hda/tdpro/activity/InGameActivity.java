c5186da8adfc6fdf394ff979c9bd462be75876fb
MarianThiel <MarianThiel@users.noreply.github.com>
2021-12-27 13:49:10 +0100

    created logic for selecting tower by adding a boolean variable

--- app/src/main/java/de/hda/tdpro/activity/InGameActivity.java
+++ app/src/main/java/de/hda/tdpro/activity/InGameActivity.java
@@ -2,9 +2,12 @@ package de.hda.tdpro.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
 
 import de.hda.tdpro.R;
 import de.hda.tdpro.core.DemoView;
+import de.hda.tdpro.core.tower.TowerType;
 
 public class InGameActivity extends AppCompatActivity {
     private DemoView gameView;
@@ -12,8 +15,11 @@ public class InGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        gameView = new DemoView(this);
-        setContentView(gameView);
+        //gameView = new DemoView(this);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_in_game);
+        gameView = findViewById(R.id.view);
+
     }
 
     @Override
