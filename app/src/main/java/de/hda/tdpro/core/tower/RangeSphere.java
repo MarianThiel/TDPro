2c000f38a1f5aa0979dd46b928d8977b2ed2949d
Marian Thiel <stmithie@stud.h-da.de>
2021-12-30 13:44:26 +0100

    commented FireTower

--- app/src/main/java/de/hda/tdpro/core/tower/RangeSphere.java
+++ app/src/main/java/de/hda/tdpro/core/tower/RangeSphere.java
@@ -13,20 +13,44 @@ import de.hda.tdpro.core.tower.priority.EnemyHPMaxComparator;
 import de.hda.tdpro.core.tower.priority.EnemyHPMinComparator;
 import de.hda.tdpro.core.tower.priority.Priority;
 
+/**
+ * @author Marian Thiel
+ * @version 1.2
+ * RangeSphere is a part of Tower it holds all enemies which are in range and process them by setting the HP
+ * if an Enemy has Zero HP or it get out of range it will be removed from the Sphere
+ * the Sphere has a priority which enemy has to be processed
+ */
 public class RangeSphere implements Drawable {
-
+    /**
+     * range of the sphere
+     */
     private final int range;
-
+    /**
+     * PriorityQueue to hold enemies and process them at periodic timestamps
+     */
     private final PriorityQueue<Enemy> queue;
-
+    /**
+     * Comparator to set the sorting order in PriorityQueue
+     */
     private final Comparator<Enemy> cmp;
-
+    /**
+     * not used yet
+     */
     private ShootingBehavior shootingBehavior;
-
+    /**
+     * according tower
+     */
     private final Tower tower;
-
+    /**
+     * Projectile to draw
+     */
     Projectile projectile;
 
+    /**
+     * basic constructor
+     * @param t according tower
+     * @param range range as integer
+     */
     public RangeSphere(Tower t, int range) {
         this.range = range;
         cmp = new EnemyHPMinComparator();
@@ -35,14 +59,28 @@ public class RangeSphere implements Drawable {
 
     }
 
+    /**
+     * hits enemy of queue
+     * can be extended to hit enemy by a specific ShootingBehavior
+     *
+     * @param dmg damage to deal
+     */
     public void hitEnemy(int dmg){
         Enemy e = queue.peek();
-        projectile =  new Projectile(tower.pos.getxVal(),tower.pos.getyVal(),e.getPosition().getxVal(),e.getPosition().getyVal(),0,null);
-        e.setHp(e.getHp()-dmg);
-        if(e.getHp()<=0){
-            queue.poll();
-            Log.println(Log.ASSERT,"enemy_targeting", "ENEMY_WAS_DEFEATED");
+        if(!removeDeadEnemy(e)){
+            projectile =  new Projectile(tower.pos.getxVal(),tower.pos.getyVal(),e.getPosition().getxVal(),e.getPosition().getyVal(),0,null);
+            e.setHp(e.getHp()-dmg);
         }
+
+        removeDeadEnemy(e);
+    }
+
+    private boolean removeDeadEnemy(Enemy e){
+        if(e.getHp() <= 0){
+            queue.poll();
+            return true;
+        }
+        return false;
     }
 
     public boolean intersects(Position p){
